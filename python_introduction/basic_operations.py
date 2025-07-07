e719397b4e01dd2c777c30e16cd1bea054388c95
Kwingah <kwingahmary@gmail.com>
2025-07-07 14:57:57 +0300

    Add all Python introduction tasks

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -10,3 +10,5 @@ multiplication = number1 * number2
 print(f"Addition of {number1} and {number2} is {addition}")
 print(f"Subtraction of {number1} and {number2} is {subtraction}")
 print(f"Multiplication of {number1} and {number2} is {multiplication}")
+
+
